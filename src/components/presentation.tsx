e1228015801e6e2cdb5fcf52cef1aed476fc8be0
Ist4lri <pierreloic.gouttebel@gmail.com>
2022-08-17 15:21:59 +0200

    Enlèvement du blank

--- src/components/presentation.tsx
+++ src/components/presentation.tsx
@@ -4,7 +4,7 @@ function Presentation() {
             textAlign: "center",
         }}>
             Ceci est un petit pokédex, effectué avec une API. Vous pouvez retrouver cette API en cliquant sur ce
-             <a href="https://pokeapi.co/" title="PokéAPI" target="_blank" rel="noopener"> lien</a> 
+             <a href="https://pokeapi.co/" title="PokéAPI" rel="noopener"> lien</a> 
             <br></br>
         Pour utiliser ce pokédex, entrez le nom anglais du pokémon dont vous voulez avoir les informations, et il apparaitra sur la partie centrale de l&apos;écran.
         </p>
